fbbb74fd7c459e57cbbbe6c9d672eb4a14365055
Licence2Info <58440546+Licence2Info@users.noreply.github.com>
2019-12-13 01:28:23 +0100

    Update Monstre.java

--- Monstre.java
+++ Monstre.java
@@ -1,9 +1,9 @@
 package JeuxLabyrinthe;
 
 public class Monstre extends Individu {
+	/*
 	
-	private int force;
-
+	private int force
 	public Monstre(Piece p, int num, Position pos,int force)
 	{
 		super(p, num, pos);
@@ -22,6 +22,27 @@ public class Monstre extends Individu {
 	{
 		return force;
 	}
+	*/
+	private int forceMonstre;
 	
+	public Monstre(Piece piece, int forceMonstre) {
+		super(piece);
+		this.forceMonstre = forceMonstre;
+	}
 	
+	public Monstre(Piece piece) {
+		this(piece, (int)(Math.random()*5+1)); // Ici forceMonstre est un nombre choisi aléatoirement entre 1 et 5
+	}
+
+
+	@Override
+	public void executeOp(Joueur j) {
+		if(this.getNbOpmax()>0) {
+			j.decrPtForce(forceMonstre);
+			super.decrNbOpMax();
+//			Après avoir donné des coups au Joueur, le monstre diparaît
+			this.getMaCase().resetObjet();
+			this.setMaCase(null);
+		}
+	}
 }
