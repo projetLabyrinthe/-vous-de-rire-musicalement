715640334a2320dab25af2b5c24f3e4119d3652f
Licence2Info <58440546+Licence2Info@users.noreply.github.com>
2019-12-13 01:16:31 +0100

    Update Cle.java

--- Cle.java
+++ Cle.java
@@ -2,7 +2,6 @@ package JeuxLabyrinthe;
 
 public class Cle extends Objet {
 private int numeroP;
-
     public Cle(Piece p, int numero, Position po,int numeroP) {
 		super(p, numero, po);
 	}
@@ -17,4 +16,11 @@ private int numeroP;
     public String toString() {
         return super.toString();
     }
+	public void executeOp(Joueur j) {
+		super.addToPlayerObjects(j);
+		this.piece.setOuvertN(true);
+		this.piece.setOuvertS(true);
+		this.piece.setOuvertE(true);
+		this.piece.setOuvertO(true);
+	}
 }
